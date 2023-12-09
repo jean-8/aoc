8a90039fc242c5d65df2cce7afe75203ed45c905
jean-8 <github@jean.ovh>
2023-12-09 14:44:25 +0100

    day_09

---
+++ 2023/day_09s1.js
@@ -0,0 +1,25 @@
+with((require('readline')).createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+}))
+{
+  var map = [];
+
+  on('line', (line) =>
+    {
+      map.push([line.split(' ').map(x => Number(x))]);
+      for (var i = 0; !map[map.length - 1][i].reduce((a, b) => a && b == 0, true); ++i)
+      {
+	map[map.length - 1].push([]);
+	for (var j = 0; j < map[map.length - 1][i].length - 1; ++j)
+	  map[map.length - 1][i + 1][j] = map[map.length - 1][i][j + 1] - map[map.length - 1][i][j];
+      }
+    });
+  once('close', () => {
+    var s1 = 0;
+
+    map.forEach(e => s1 += e.reduce((a, b) => a += b[b.length - 1], 0));
+    console.log(s1);
+  });
+};
